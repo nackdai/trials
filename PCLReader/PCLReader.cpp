86edb18671b412eadf61ebcb05ff87f3df2229b9
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-03-09 14:49:42 +0900

    Link with potree.

--- PCLReader/PCLReader.cpp
+++ PCLReader/PCLReader.cpp
@@ -3,9 +3,19 @@
 
 #include "stdafx.h"
 
+#include "proxy.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    std::string path(argv[1]);
+
+    auto reader = Proxy::createPointReader(path);
+
+
+
+    reader->close();
+    delete reader;
+
 	return 0;
 }
 
