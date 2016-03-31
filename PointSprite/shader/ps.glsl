f8b4e3d62358ca04068a313df1a4fcf69b43e3ba
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-03-31 13:17:37 +0900

    Draw point sprites as circle.

--- PointSprite/shader/ps.glsl
+++ PointSprite/shader/ps.glsl
@@ -6,6 +6,18 @@ varying vec4 var_Color;
 
 void main()
 {
+    vec3 n;
+
+    // 座標値を [0, 1] → [-1, 1] に変換する.
+    n.xy = gl_PointCoord * 2.0 - 1.0;
+
+    // 半径１の円の中にあるかどうか.
+    n.z = 1.0 - dot(n.xy, n.xy);
+    if (n.z < 0.0) {
+        // 結果が負なら円の外にあるのでフラグメントを捨てる.
+        discard;
+    }
+
     gl_FragColor = var_Color;
     //gl_FragColor = vec4(1.0);
 }
